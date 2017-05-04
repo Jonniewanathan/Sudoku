9ccf47640da5241990f32701392f6e3ce3473922
Jonathan Quirke <jonathan41@hotmail.com>
2017-05-04 03:22:48 +0100

    Added onClicklisteners to the settings switches

--- app/src/main/java/com/jonniewanathan/sudoku/SettingsActivity.java
+++ app/src/main/java/com/jonniewanathan/sudoku/SettingsActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     private Button resetPuzzles;
+    private Switch music;
+    private Switch sound;
+    private Switch hint;
     private DBHandler dbHandler;
 
 
@@ -20,6 +24,9 @@ public class SettingsActivity extends AppCompatActivity {
         dbHandler = new DBHandler(this);
 
         resetPuzzles = (Button) findViewById(R.id.reset_button);
+        hint = (Switch) findViewById(R.id.auto_hint);
+        music = (Switch) findViewById(R.id.music);
+        sound = (Switch) findViewById(R.id.sound);
 
 
         resetPuzzles.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +38,25 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        hint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Not currently implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Not currently implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Not currently implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
